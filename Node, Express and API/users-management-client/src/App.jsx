e352b39555cbfde2a1f264eb547555dce3847f68
Tonmoy <tonmoy@Tonmoy.Tonmoy>
2026-01-16 21:17:18 +0600

    client and server interaction

--- Node, Express and API/users-management-client/src/App.jsx
+++ Node, Express and API/users-management-client/src/App.jsx
@@ -1,12 +1,18 @@
 
 import './App.css'
+import Users from './componenets/Users';
+
+const userPromise = fetch('http://localhost:3000/users').then(res => res.json());
 
 function App() {
+
 
   return (
     <>
 
       <h1>Users Management Application</h1>
+
+      <Users userPromise={userPromise} ></Users>
 
     </>
   )
--- Node, Express and API/users-management-server/index.js
+++ Node, Express and API/users-management-server/index.js
@@ -1,9 +1,35 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = process.env.PORT || 3000;
 
+app.use(cors())
+app.use(express.json());
+
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
+})
+
+const users = [
+    { id: 1, name: 'Sohag', email: 'sohag@gmail.com' },
+    { id: 2, name: 'Shuvo', email: 'shuvo@gmail.com' },
+    { id: 3, name: 'Joy', email: 'joy@gmail.com' }
+]
+
+app.get('/users', (req, res) => {
+    res.send(users)
+})
+
+app.post('/users', (req, res) => {
+    console.log("Users post method");
+    console.log(req.body);
+    const newUser = req.body;
+    newUser.id = users.length + 1;
+
+    users.push(newUser);
+
+    res.send(newUser);
 })
 
 app.listen(port, () => {
