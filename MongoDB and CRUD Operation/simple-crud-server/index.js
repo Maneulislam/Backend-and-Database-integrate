227da44ef8043149faae47e4642226507c4baaed
Tonmoy <tonmoy@Tonmoy.Tonmoy>
2026-01-20 22:48:43 +0600

    add server and client

--- MongoDB and CRUD Operation/simple-crud-server/index.js
+++ MongoDB and CRUD Operation/simple-crud-server/index.js
@@ -1,13 +1,42 @@
 const express = require('express')
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
-//User name : simpleCrudDB
-//pass : g7.pG@7jBi#YWNu
+//User name : 221002199_db_user
+//pass : ihWab9So5M3eWWt5
+
+
+const uri = "mongodb+srv://221002199_db_user:ihWab9So5M3eWWt5@simple-crud-server.awx9wzo.mongodb.net/?appName=simple-crud-server";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
 
 app.get('/', (req, res) => {
     res.send('Simple crud server is Running...')
